b4fc4472ee997f330bf22768902179aa716e3b36
Hemadri Varma <varmahemadri@gmail.com>
2026-07-06 10:58:50 +0530

    Uploaded resume upload feature

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import os
 from werkzeug.utils import secure_filename
-UPLOAD_FOLDER = "static/resumes"
-app.config["UPLOAD_FOLDER"]=UPLOAD_FOLDER
 from flask import Flask, render_template, request, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'placement123'
 
+app.config["UPLOAD_FOLDER"] = "static/resumes"
+app.config['SECRET_KEY'] = 'placement123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placement.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
